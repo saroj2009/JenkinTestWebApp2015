6944eb0e420332fa064f65a875dd95a0fe3af78f
Saroj Parida <SarojP@cogcis.onmicrosoft.com>
2019-01-30 13:42:09 +0000

    Add project files.

---
+++ JenkinTestWebApp2015/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(JenkinTestWebApp2015.Startup))]
+namespace JenkinTestWebApp2015
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
